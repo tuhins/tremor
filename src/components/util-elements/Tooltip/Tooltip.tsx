c914a5889a3d6de4fc92f03333524206bfb16667
amirhhashemi <87268103+amirhhashemi@users.noreply.github.com>
2023-05-27 12:22:19 +0330

    fix(tooltip): set max width for the tooltip (#455)

--- src/components/util-elements/Tooltip/Tooltip.tsx
+++ src/components/util-elements/Tooltip/Tooltip.tsx
@@ -70,7 +70,7 @@ const Tooltip = ({ text, open, x, y, refs, strategy, getFloatingProps }: Tooltip
   return open && text ? (
     <div
       className={twMerge(
-        "w-max text-sm z-20",
+        "max-w-xs text-sm z-20",
         getColorClassNames(DEFAULT_COLOR, colorPalette.darkestBackground).bgColor,
         getColorClassNames("white").textColor,
         borderRadius.md.all,
